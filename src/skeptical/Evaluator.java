c2bd77d29b05520704897cac204bcbff88b74297
Gabriel Speer <gspeer2@ut.utm.edu>
2025-04-16 10:59:27 -0500

    Update Evaluator.java

--- src/skeptical/Evaluator.java
+++ src/skeptical/Evaluator.java
@@ -8,3 +8,42 @@ import java.io.File;
 import java.io.IOException;
 
 import skeptical.Env.*;
+
+
+public class Evaluator implements Visitor<Value> {
+	
+	Printer.Formatter ts = new Printer.Formatter();
+
+	Env initEnv = initialEnv(); //New for definelang
+	
+	Value valueOf(Program p) {
+			return (Value) p.accept(this, initEnv);
+	}
+
+  private Env initialEnv() {
+		GlobalEnv initEnv = new GlobalEnv();
+		
+		/* Procedure: (read <filename>). Following is same as (define read (lambda (file) (read file))) */
+		List<String> formals = new ArrayList<>();
+		formals.add("file");
+		Exp body = new AST.ReadExp(new VarExp("file"));
+		Value.FunVal readFun = new Value.FunVal(initEnv, formals, body);
+		initEnv.extend("read", readFun);
+
+		/* Procedure: (require <filename>). Following is same as (define require (lambda (file) (eval (read file)))) */
+		formals = new ArrayList<>();
+		formals.add("file");
+		body = new EvalExp(new AST.ReadExp(new VarExp("file")));
+		Value.FunVal requireFun = new Value.FunVal(initEnv, formals, body);
+		initEnv.extend("require", requireFun);
+		
+		/* Add new built-in procedures here */ 
+		
+		return initEnv;
+	}
+	
+	Reader _reader; 
+	public Evaluator(Reader reader) {
+		_reader = reader;
+	}
+}
