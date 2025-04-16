8d50e542a36f99e6100d355e01a7d4acf8371739
Gabriel Speer <gspeer2@ut.utm.edu>
2025-04-15 23:21:06 -0500

    Create Evaluator.java
    starting evaluator file

---
+++ src/skeptical/Evaluator.java
@@ -0,0 +1,10 @@
+package skeptical;
+import static skeptical.AST.*;
+import static skeptical.Value.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+import skeptical.Env.*;
