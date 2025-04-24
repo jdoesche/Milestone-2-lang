18e4f53370d3935bd5053cf678bbf3bd9d0dbd34
Gabriel Speer <gspeer2@ut.utm.edu>
2025-04-24 13:30:50 -0500

    Create Interpreter.java

---
+++ src/skeptical/Interpreter.java
@@ -0,0 +1,38 @@
+package skeptical;
+import java.io.IOException;
+
+import skeptical.Env;
+import skeptical.Value;
+import skeptical.AST.*;
+
+/**
+ * This main class implements the Read-Eval-Print-Loop of the interpreter with
+ * the help of Reader, Evaluator, and Printer classes. 
+ * 
+ * @author hridesh
+ *
+ */
+public class Interpreter {
+	public static void main(String[] args) {
+		System.out.println("Type a program to evaluate and press the enter key," +
+							"Press Ctrl + C to exit.");
+		Reader reader = new Reader();
+		Evaluator eval = new Evaluator(reader);
+		Printer printer = new Printer();
+		REPL: while (true) { // Read-Eval-Print-Loop (also known as REPL)
+			Program p = null;
+			try {
+				p = reader.read();
+				if(p._e == null) continue REPL;
+				Value val = eval.valueOf(p);
+				printer.print(val);
+			} catch (Env.LookupException e) {
+				printer.print(e);
+			} catch (IOException e) {
+				System.out.println("Error reading input:" + e.getMessage());
+			} catch (NullPointerException e) {
+				System.out.println("Error:" + e.getMessage());
+			}
+		}
+	}
+}
